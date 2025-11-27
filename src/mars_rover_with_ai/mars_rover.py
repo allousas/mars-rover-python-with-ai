a5155f968d293e54ef462c35695dfec3e3bce2f8
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2025-11-27 14:19:48 +0100

    Use a data class for position

--- src/mars_rover_with_ai/mars_rover.py
+++ src/mars_rover_with_ai/mars_rover.py
@@ -1,16 +1,15 @@
-class Position:
-    def __init__(self, x, y, direction):
-        self.x = x
-        self.y = y
-        self.direction = direction
-
-    def __eq__(self, other):
-        if not isinstance(other, Position):
-            return False
-        return self.x == other.x and self.y == other.y and self.direction == other.direction
+from dataclasses import dataclass
 
 
 class MarsRover:
     @property
     def position(self):
         return Position(0, 0, 'N')
+
+
+@dataclass
+class Position:
+    x: int
+    y: int
+    direction: str
+
