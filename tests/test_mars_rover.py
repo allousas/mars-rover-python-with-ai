192eca7722bd0cca936070e2e01ed155cad067a1
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2025-11-28 14:19:59 +0100

    Test case scenario: rovers should turn right

--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -13,3 +13,11 @@ def test_should_move_forward():
     rover.execute(command='f')
 
     assert rover.position == Position(0, 1, Direction.N)
+
+
+def test_should_turn_right():
+    rover = MarsRover()
+    rover.execute(command='r')
+
+    assert rover.position == Position(0, 0, Direction.E)
+
