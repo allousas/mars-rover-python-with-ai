3745580d4186cda79528b05bb1461fd67b356a93
Albert Llousas Ortiz <abert.ortizl@gmail.com>
2025-12-02 16:47:13 +0100

    Refactor test case scenario: mars initialisation

--- src/mars_rover_with_ai/mars_rover.py
+++ src/mars_rover_with_ai/mars_rover.py
@@ -1,10 +1,17 @@
-from mars_rover_with_ai.position import Position, Direction, N, E, S, W
+from typing import List
+from mars_rover_with_ai.position import Position
 
 
 class MarsRover:
-    def __init__(self, position=None):
-        self._position = position if position is not None else Position(0, 0, N)
+    def __init__(self, position: Position, grid_map: List[str]):
+        self._position: Position = position
+        self._grid_map: List[str] = grid_map
 
     @property
     def position(self):
         return self._position
+
+    @property
+    def grid_map(self):
+        return self._grid_map
+
--- tests/test_mars_rover.py
+++ tests/test_mars_rover.py
@@ -4,8 +4,16 @@ from mars_rover_with_ai.position import Position, Direction
 N, E, S, W = Direction.N, Direction.E, Direction.S, Direction.W
 
 
-def test_should_initialize_at_default_position():
-    rover = MarsRover()
-    position = rover.position
+def test_should_initialise_with_an_initial_position_and_a_map():
+    grid_map = [
+        '🟩🟩🌳🟩🟩',
+        '🟩🟩🟩🟩🟩',
+        '🟩🟩🟩🌳🟩',
+        '🟩🌳🟩🟩🟩',
+        '🟩🟩🟩🟩🟩'
+    ]
+    initial_position = Position(0, 4, E)
+    rover = MarsRover(position=initial_position, grid_map=grid_map)
 
-    assert position == Position(0, 0, N)
+    assert rover.position == initial_position
+    assert rover.grid_map == grid_map
